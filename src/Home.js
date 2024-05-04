58f669226ce554baa79cc585b73bcc6f425f6797
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2024-05-04 16:38:57 +0530

    Added more sub-parts in the Grid of home page

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,11 @@ import axios from 'axios'
 import React, { useState, useEffect } from 'react';
 import Paper from '@mui/material/Paper';
 import { Grid } from '@mui/material';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import './App.css';
 
 const Home = () => {
 
@@ -34,7 +39,7 @@ const Home = () => {
     if (!data) return <p>Loading...</p>;
 
     return (
-        <div>
+        <div className='grids'>
             <h2>data</h2>
             <>
                 <Grid container spacing={2}>  {/* Add spacing between grid items */}
@@ -42,7 +47,32 @@ const Home = () => {
                         return (
                             <Grid item xs={12} md={6} lg={4} key={id}>
                                 <Paper variant="elevation">
-                                    {item.jobRole}
+                                    {/* {item.jobRole} */}
+                                    <CardContent className='card'>
+                                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                            <p>Estimated Salary: {item.minJdSalary} - {item.maxJdSalary} LPA<span aria-label="Offered salary range" class=""> ✅</span><br /></p>
+                                        </Typography>
+                                        <Typography variant="h5" component="div">
+                                            <p class="MuiTypography-root MuiTypography-body1 css-1hw7dw8">About Company:</p>
+                                        </Typography>
+                                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                            <div>
+                                                <p>About us:</p>
+                                                <p>{item.jobDetailsFromCompany}</p>
+                                            </div>
+                                        </Typography>
+                                        <Typography variant="body2">
+                                            <div>
+                                                <h3>Minimum experience</h3>
+                                                <h4>{item.minExp} Years</h4>
+                                            </div>
+                                        </Typography>
+                                        <Typography>
+                                            <div className='btn-div'>
+                                                <Button className='btn'>⚡ Easy Apply</Button>
+                                            </div>
+                                        </Typography>
+                                    </CardContent>
                                 </Paper>
                             </Grid>
 
